9846e0c57109450c156139d9d8a20161e7f0c047
Manar Jber <107268660+Manar960@users.noreply.github.com>
2023-01-11 18:42:39 +0200

    Add files via upload
    JavaScript course project

---
+++ pro.js
@@ -0,0 +1,31 @@
+
+let a=document.getElementById("home-score")
+let b=document.getElementById("guest-score")
+let count =0
+let count1=0
+function add1a(){
+    count+=1
+    a.innerHTML=count
+}
+function add2a(){
+    count+=2
+    a.innerHTML=count
+   
+}
+function add3a(){
+    count+=3
+    a.innerHTML=count
+}
+function add1b(){
+count1+=1
+b.innerHTML=count1
+}
+function add2b(){
+count1+=2
+b.innerHTML=count1
+}
+function add3b(){
+count1+=3
+b.innerHTML=count1
+}
+
